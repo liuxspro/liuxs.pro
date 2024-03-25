e6488be9a35b8fb8cdb27f1f73ed8c0bd803d150
liuxsdev <liuxspro@gmail.com>
2024-03-25 21:29:52 +0800

    Chore: move to astro

--- content/syy/_index.md
+++
@@ -1,7 +0,0 @@
-+++
-title = "书·影·音"
-sort_by = "date"
-template = "syy.html"
-page_template = "blog-page.html"
-paginate_by = 6
-+++
---
+++ src/content/config.ts
@@ -0,0 +1,15 @@
+// 从 `astro:content` 导入辅助工具
+import { z, defineCollection } from "astro:content";
+// 为每一个集合定义一个 `type` 和 `schema`
+const postsCollection = defineCollection({
+  type: "content",
+  schema: z.object({
+    title: z.string(),
+    date: z.date(),
+    tags: z.array(z.string()).default(["others"]),
+  }),
+});
+// 导出一个单独的 `collections` 对象来注册你的集合
+export const collections = {
+  blog: postsCollection,
+};
