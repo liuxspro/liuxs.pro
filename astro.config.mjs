c136afa9f04774282cbed5f7b0477ed52764d2a5
liuxspro <liuxspro@gmail.com>
2024-05-24 17:35:45 +0800

    Chore: add rehype-external-links

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,5 +1,6 @@
 import { defineConfig } from "astro/config";
 import remarkMath from "remark-math";
+import rehypeExternalLinks from "rehype-external-links";
 import rehypeKatex from "rehype-katex";
 import tailwind from "@astrojs/tailwind";
 import mdx from "@astrojs/mdx";
@@ -16,7 +17,7 @@ export default defineConfig({
   integrations: [tailwind(), mdx()],
   markdown: {
     remarkPlugins: [remarkMath],
-    rehypePlugins: [rehypeKatex],
+    rehypePlugins: [rehypeKatex, [rehypeExternalLinks, { rel: ["noopener", "noreferrer"], target: ["_blank"] }]],
     shikiConfig,
     // 示例：将脚注文本翻译成另一种语言，这里是默认的英文内容
     remarkRehype: {
