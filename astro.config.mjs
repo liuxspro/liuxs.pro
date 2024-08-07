38a8a7de3407b3d9c0fa8d80f08723837f5767f4
liuxspro <liuxspro@gmail.com>
2024-08-07 17:56:25 +0800

    添加vue集成

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,28 +4,33 @@ import rehypeExternalLinks from "rehype-external-links";
 import rehypeKatex from "rehype-katex";
 import tailwind from "@astrojs/tailwind";
 import mdx from "@astrojs/mdx";
+import vue from "@astrojs/vue";
 const shikiConfig = {
   themes: {
     light: "vitesse-light",
-    dark: "solarized-dark",
+    dark: "solarized-dark"
   },
-  wrap: true,
+  wrap: true
 };
+
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [tailwind(), mdx()],
+  integrations: [tailwind(), mdx(), vue()],
   markdown: {
     remarkPlugins: [remarkMath],
-    rehypePlugins: [rehypeKatex, [rehypeExternalLinks, { rel: ["noopener", "noreferrer"], target: ["_blank"] }]],
+    rehypePlugins: [rehypeKatex, [rehypeExternalLinks, {
+      rel: ["noopener", "noreferrer"],
+      target: ["_blank"]
+    }]],
     shikiConfig,
     // 示例：将脚注文本翻译成另一种语言，这里是默认的英文内容
     remarkRehype: {
       footnoteLabel: "参考",
-      footnoteBackLabel: "Back to reference 1",
-    },
+      footnoteBackLabel: "Back to reference 1"
+    }
   },
   devToolbar: {
-    enabled: false,
-  },
+    enabled: false
+  }
 });
